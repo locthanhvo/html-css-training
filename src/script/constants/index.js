4bee242a45f26a44c9fddaee46d1e0a3bcbdf355
Loc Thanh Vo <loc.vo@asnet.com.vn>
2023-10-17 14:13:22 +0700

    remove repo js

--- src/script/constants/index.js
+++
@@ -1,4 +0,0 @@
-export * from './common';
-export * from './message';
-export * from './regex';
-export * from './form';
--- src/script/utils/index.js
+++
@@ -1,7 +0,0 @@
-export * from './formatDate';
-export * from './httpsRequest';
-export * from './formValue';
-export * from './permissionField';
-export * from './debounce';
-export * from './buildQueryString';
-export * from './formatPhoneNumber';
